85c9e12fdc7d10f135fe61a3eeba052da990fe98
Tomas Trimboli <trimbolitomas10@gmail.com>
2026-06-07 13:13:16 +0200

    series temporales en flask

--- src/app.py
+++ src/app.py
@@ -1,28 +1,19 @@
 from flask import Flask, request, render_template
-from pickle import load
+from utils import load_model, make_forecast
+import os
 
 app = Flask(__name__)
-model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
-class_dict = {
-    "0": "Iris setosa",
-    "1": "Iris versicolor",
-    "2": "Iris virginica"
-}
 
-@app.route("/", methods = ["GET", "POST"])
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model = load_model(os.path.join(BASE_DIR, "..", "models", "series-temporales-trimboli.sav"))
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    forecast_data = None
+    steps = 30
+
     if request.method == "POST":
-        
-        # Obtain values from form
-        val1 = float(request.form["val1"])
-        val2 = float(request.form["val2"])
-        val3 = float(request.form["val3"])
-        val4 = float(request.form["val4"])
-        
-        data = [[val1, val2, val3, val4]]
-        prediction = str(model.predict(data)[0])
-        pred_class = class_dict[prediction]
-    else:
-        pred_class = None
-    
-    return render_template("index.html", prediction = pred_class)
+        steps = int(request.form["steps"])
+        forecast_data = make_forecast(model, steps)
+
+    return render_template("index.html", forecast=forecast_data, steps=steps)
